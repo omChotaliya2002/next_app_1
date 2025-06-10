2173ded268cc13e8ea362c250e690549487ad119
omChotaliya2002 <omchotaliya2005@gmail.com>
2025-06-10 18:18:11 +0530

    make some changes

--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-interface ProductPageProps {
+interface ProductPrams {
     productid : string;
   };
 
-const Page = (params : ProductPageProps) => {
+interface ProductPageProps {
+  params : ProductPrams;
+}
+
+const Page = ({params} : ProductPageProps) => {
+
+  const {productid} = params;
 
   return (
 
@@ -12,7 +18,7 @@ const Page = (params : ProductPageProps) => {
     <div className='flex flex-col items-center justify-center mt-[10%]'>
            <h1 className='underline underline-offset-[5px] font-semibold text-[18px]'> Detailes about product : </h1> 
 
-          <h1 className='mt-[1%]'> id : {params.productid}; </h1>  
+          <h1 className='mt-[1%]'> id : {productid}; </h1>  
            <p> Price : 2000/- </p> 
            <p className='font-bold'> Made in India </p> 
     </div>
